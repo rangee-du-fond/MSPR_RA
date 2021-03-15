d9e29f512b161a3040d371441bd48a3e571763cf
Th30phaN3 <theophane.pro@tuta.io>
2021-03-15 11:47:12 +0100

    Add logo + share button

---
+++ Assets/ShareButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.IO;
+using UnityEngine;
+
+public class ShareButton : MonoBehaviour
+{
+    public void ClickShareButton()
+    {
+        StartCoroutine(TakeScreenshotAndShare());
+    }
+
+	private IEnumerator TakeScreenshotAndShare()
+    {
+        yield return new WaitForEndOfFrame();
+
+        Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        ss.Apply();
+
+        string filePath = Path.Combine(Application.temporaryCachePath, "mspr_ra.png");
+        File.WriteAllBytes(filePath, ss.EncodeToPNG());
+
+        // To avoid memory leaks
+        Destroy(ss);
+
+        new NativeShare().AddFile(filePath)
+            .SetSubject("MSPR_RA").SetText("See my wonderful friend from Céréalis !")
+            .SetCallback((result, shareTarget) => Debug.Log("Share result: " + result + ", selected app: " + shareTarget))
+            .Share();
+    }
+}
